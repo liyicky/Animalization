e4d9a629431de2df1a2a1d2d6aa141030a69d6eb
liyicky <jason@liyicky.com>
2023-03-09 15:00:59 +0900

    Initial Commit

---
+++ Animalization/AnimalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalizationApp.swift
+//  Animalization
+//
+//  Created by Jason Cheladyn on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct AnimalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animalization/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Animalization
+//
+//  Created by Jason Cheladyn on 2023/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
